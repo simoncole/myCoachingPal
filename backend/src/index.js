d5646890be7e6405fb0465c00be69b85048d1094
Simon Cole <simonccole1@gmail.com>
2023-02-01 11:48:38 -0500

    added environment variables package and configured test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 
 const port = 4000;
+const testKey = process.env.TEST;
 const app = express();
 
 app.get("/", (req, res) => {
+    console.log(testKey);
     res.send("Start of myCoachingPal backend!");
 })
 

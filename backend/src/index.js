f42ccf8bc87c6ab3ea609d934cf03155f48008f5
Simon Cole <simonccole1@gmail.com>
2023-02-02 13:21:51 -0500

    added endpoint to query db and verify the user based on their username and password

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,11 +10,32 @@ const app = express();
 const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
 app.get("/", async (req, res) => {
-    const [rows, fields] = await connection.execute("SELECT * FROM users;");
+    res.status(200).json({"data": "start of backend!"})
+});
+
+app.get("/verifyUser", async (req, res) => {
+    const username = req.query.username;
+    const password = req.query.password;
+
+    //query the database
+    const queryString = "SELECT * FROM users WHERE username=?"
+    try{
+        const [rows, fields] = await connection.execute(queryString, [username]);
+        //verify the data matches and return their role
+        if(rows[0].username === username && rows[0].password === password){
+            res.status(200).json({"role": rows[0].role});
+        }
+        else res.status(200).json({"role": "unverified"});
+        
+    }
+    catch(err){
+        if(err.message = "Cannot read properties of undefined (reading 'username')"){
+            res.status(200).json({"role": "unverified"})
+        }
+        res.status(500).json({"error": err.message});
+    }
     
-    console.log(rows);
-    res.send("Start of myCoachingPal backend!");
-})
+});
 
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
